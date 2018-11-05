00363cf7665eb4a84318ae95a3a700151a5a0f7f
Oumayma Jellite <ojellite@emi.u-bordeaux1.fr>
2018-11-05 17:11:28 +0100

    td2-partie2

---
+++ code/test/seulthread.c
@@ -0,0 +1,16 @@
+#include "syscall.h"
+
+
+void print(char c){
+  int i=0;
+  for(i=0;i<3;i++)
+  { PutChar(c+i); }
+    PutChar('\n');
+    ThreadExit();
+}
+int main()
+{
+  ThreadCreate((void *)print,'a');
+  PutChar('m');
+  ThreadExit();
+}
